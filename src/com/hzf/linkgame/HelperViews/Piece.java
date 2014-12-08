17bf9129d66bfab24e9a2af43bbcb043e1f04fd8
huangzhifei <huangzhifei@skyworth.com>
2014-12-08 17:21:10 +0800

    添加辅助view

---
+++ src/com/hzf/linkgame/HelperViews/Piece.java
@@ -0,0 +1,9 @@
+package com.hzf.linkgame.HelperViews;
+
+/*
+ * 方块对象辅助类
+ */
+public class Piece {
+	
+
+}
---
+++ src/com/hzf/linkgame/HelperViews/PieceImage.java
@@ -0,0 +1,8 @@
+package com.hzf.linkgame.HelperViews;
+
+/*
+ * bitmap & id
+ */
+public class PieceImage {
+
+}
