60a7ec8fb903a656c8d56e11ac4952c085b624ff
huangzhifei <huangzhifei@skyworth.com>
2014-12-09 17:15:55 +0800

    添加 piece、GameView、pieceimage帮助类。

--- src/com/hzf/linkgame/HelperViews/Piece.java
+++ src/com/hzf/linkgame/HelperViews/Piece.java
@@ -1,9 +1,97 @@
 package com.hzf.linkgame.HelperViews;
 
+import android.graphics.Point;
+
 /*
  * 方块对象辅助类
  */
 public class Piece {
 	
-
+	//保存方块对象的所对应的图像
+	private PieceImage pieceImage;
+	
+	//方块的左上角x、y坐标
+	private int beginX, beginY;
+	
+	//对象piece[][]数组的下标
+	private int indexX, indexY;
+	
+	public Piece(int iX, int iY)
+	{
+		this.indexX = iX;
+		this.indexY = iY;
+	}
+	/*
+	 * 设置beginX的属性
+	 */
+	public final int getBeginX(){
+		return beginX;
+	}
+	
+	public void setBeginX(int iX){
+		this.beginX = iX;
+	}
+	
+	/*
+	 * 设置beginY的属性
+	 */
+	public final int getBeginY(){
+		return beginY;
+	}
+	
+	public void setBeginY(int iY){
+		this.beginY = iY;
+	}
+	
+	/*
+	 * 设置indexX的属性
+	 */
+	public final int getIndexX(){
+		return indexX;
+	}
+	
+	public void setIndexX(int iX){
+		this.indexX = iX;
+	}
+	
+	/*
+	 * 设置indexY的属性
+	 */
+	public final int getIndexY(){
+		return indexY;
+	}
+	
+	public void setIndexY(int iY){
+		this.indexY = iY;
+	}
+	
+	/*
+	 * 设置pieceImage属性
+	 */
+	public final PieceImage getImage(){
+		return pieceImage;
+	}
+	
+	public void setImage(PieceImage image){
+		this.pieceImage = image;
+	}
+	
+	//获取piece的中心点
+	public Point getCenter()
+	{
+		double x = getImage().getImage().getWidth() * 0.5 + getBeginX();
+		double y = getImage().getImage().getHeight() * 0.5 + getBeginY();
+		
+		return new Point((int)x, (int)y);
+	}
+	
+	public boolean isSameImage(Piece rhs)
+	{
+		if( pieceImage == null && rhs.pieceImage != null )
+		{
+			return false;
+		}
+		
+		return pieceImage.getImageID() == rhs.getImage().getImageID();
+	}
 }
--- src/com/hzf/linkgame/HelperViews/PieceImage.java
+++ src/com/hzf/linkgame/HelperViews/PieceImage.java
@@ -1,8 +1,38 @@
 package com.hzf.linkgame.HelperViews;
 
+import android.graphics.Bitmap;
+
 /*
  * bitmap & id
  */
 public class PieceImage {
 
+	private Bitmap imageBitmap;
+	
+	private int imageID;
+	
+	public PieceImage(Bitmap image, int imageID)
+	{
+		this.imageBitmap = image;
+		this.imageID = imageID;
+	}
+	
+	public final Bitmap getImage(){
+		return imageBitmap;
+	}
+	
+	public void setImage(Bitmap image)
+	{
+		this.imageBitmap = image;
+	}
+	
+	public final int getImageID()
+	{
+		return imageID;
+	}
+	
+	public void setImageID(int imageID)
+	{
+		this.imageID = imageID;
+	}
 }
